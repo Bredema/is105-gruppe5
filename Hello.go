e54b28fcbd5f8f11b984132817663247e69185dd
hyttapower <mortjm14@student.uia.no>
2017-01-26 14:39:50 +0100

    Update Hello.go

--- Hello.go
+++ Hello.go
@@ -3,7 +3,7 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, 世界")
 	fmt.Println("Mikael var her")
-	fmt.Println("Morten endret dette")
+	fmt.Println("Morten endret dette, wiii")
 	fmt.Println("Hei alle sammen")
 	fmt.Println("Her er enda en endring")
 	fmt.Println("Eirik sier hallo") // KOMMENTAR
