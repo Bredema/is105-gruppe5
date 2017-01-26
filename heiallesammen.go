89ca05b6ee02efd1ab67e8ddff270f236be1cc2d
Mikael Kimerud <Mikael Kimerud>
2017-01-26 14:10:12 +0100

    Slettet unødvendige filer

--- heiallesammen.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	fmt.Println("1 dag til fredag! 4 pils og 1 pizza")
-}
--- heyhaha.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	fmt.Println("HEYHAHA")
-	fmt.Println("heiheiheiendring")
-}
